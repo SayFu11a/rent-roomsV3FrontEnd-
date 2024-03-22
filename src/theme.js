858e3cf7cbc738d2dcee01473292fb4c5b97e017
confirence <diggffffg@gmail.com>
2024-03-22 18:03:58 +0300

    fix heder

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+   shadows: [
+      'none',
+      '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
+      // Другие значения теней
+   ], // заменил
+   palette: {
+      primary: {
+         main: '#4361ee',
+      },
+   },
+   typography: {
+      button: {
+         textTransform: 'none',
+         fontWeight: 400,
+      },
+   },
+});
